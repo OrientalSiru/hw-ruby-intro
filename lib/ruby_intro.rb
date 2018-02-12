f2355876e901821c59368d8f14b99f6d32fd6c8e
EC2 Default User <ec2-user@ip-172-31-3-73.us-east-2.compute.internal>
2018-02-12 09:02:22 +0000

    Homework 1 (2/12/18)

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,99 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arraySum = 0
+  arr.each{|i|arraySum+=i}
+  return arraySum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.size == 0
+	  return 0
+  elsif arr.size == 1
+	  return arr[0]
+  else
+    sum = 0
+    arr.sort!
+    arr.reverse!
+    sum = arr[0] + arr[1]
+    return sum
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  for i in 0..arr.size - 1
+	  for j in (i+1)..arr.size - 1
+	    sum = arr[i] + arr[j]
+	    if sum == n
+		    return true
+	    end
+	  end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.length <= 0
+	  return false
+  else
+	  if s[0].ord < 65 or s[0].ord >122 #string.ord gets the ascii value of a string
+	    return false
+   	else
+	    vows = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+	  return !(vows.include? (s[0]))
+	  end
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.length == 0
+    return false
+  end
+  
+  sum = 0
+  for i in 0..s.length - 1
+	  if s[i].ord < 48 || s[i].ord > 49 
+	    return false
+  	else
+	    sum += (s[i].to_i) * (2 ** (s.length - i))
+    end
+  end
+ 
+  if sum % 4 == 0
+	  return true
+  else
+	  return false
+  end
+
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  #ruby constructor
+  def initialize(isbn, price)
+	if(isbn.length < 1)
+	  raise ArgumentError.new("ISBN Cannot be an empty string!")
+	end
+	if(price <= 0.0)
+	  raise ArgumentError.new("Price needs to be more than 0.0!")
+	end
+	@isbn = isbn
+	@price = price
+  end
+  
+  #getter and setter
+  attr_reader :isbn, :price
+  attr_writer :isbn, :price
+
+  def price_as_string
+    return sprintf("$%.2f", price)
+  end
+  
 end
